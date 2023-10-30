a87e3878125bfeb27cdf3e1d7f9b9890cc3c750d
nvthong2303 <thong.nv1@cmctelecom.vn>
2023-10-30 18:03:23 +0700

    thirds commit

--- utils/decorators.py
+++ utils/decorators.py
@@ -2,4 +2,27 @@ from flask import request
 from functools import wraps
 from werkzeug.exceptions import BadRequest
 from utils.errors import BadRequestException
-from
+from utils.validator import validate_create_body, validate_update_body
+
+def validate_json(ignore_method=None):
+    def decorator(f):
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            if ignore_method is not None:
+                for method in ignore_method:
+                    if request.method == method:
+                        return f(*args, **kwargs)
+
+            try:
+                data = request.get_json()
+                if data is None:
+                    raise BadRequest
+            except BadRequest:
+                raise BadRequestException('Invalid JSON.')
+            if request.method == 'POST':
+                validate_create_body(data)
+            if request.method == 'PUT':
+                validate_update_body(data)
+            return f(*args, **kwargs)
+        return wrapper
+    return decorator
