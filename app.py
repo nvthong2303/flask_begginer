c2960ce7ca31463e49d254e46d9efeff5a5e1290
nvthong2303 <thong.nv1@cmctelecom.vn>
2023-11-01 16:35:52 +0700

    fourth commit

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import os
 from flask import Flask
+
+from blueprints.users import users_blueprint
 from database.db import db
 from flask_cors import CORS
 from dotenv import load_dotenv
-
+from utils.errors import BadRequestException
+from utils.http import bad_request, not_found, not_allowed, interal_error
 
 load_dotenv()
 
@@ -17,11 +20,23 @@ def create_app():
 
     app.register_blueprint(users_blueprint, url='/api/v1')
 
-    # @app.errorhandler(BadRequestException)
-    # def bad_request_exception(e):
-    #     return bad_request
+    @app.errorhandler(BadRequestException)
+    def bad_request_exception(e):
+        return bad_request
+
+    @app.errorhandler(404)
+    def route_not_found(e):
+        return not_found('route')
+
+    @app.errorhandler(405)
+    def method_not_allowed(e):
+        return not_allowed()
+
+    @app.errorhandler(Exception)
+    def internal_server_error(e):
+        return interal_error(e)
 
     return app
 
 
-app = create_app(app)
+app = create_app()
--- blueprints/users.py
+++ blueprints/users.py
@@ -1,4 +1,39 @@
 from  database.db import db
+from database.helper import update_user, delete_user, get_users, create_user
 from models.models import User
 from flask import Blueprint, request
-from utils.decorators import BadRequestException
+from utils.decorators import BadRequestException, validate_json
+from utils.http import not_found, ok, no_content, created
+
+users_blueprint = Blueprint('user_blueprint', __name__)
+
+@users_blueprint.route('/user/<id>', methods=['GET', 'PUT', 'DELETE'])
+@validate_json(ignore_method=['GET', 'DELETE'])
+def user(id):
+    user_exists = User.query.get(id)
+    if user_exists is None:
+        return not_found('user')
+
+    if request.method == 'GET':
+        return ok(user_exists)
+
+    if request.method == 'PUT':
+        body = request.get_json()
+        update_user(db, user_exists, body)
+        return no_content()
+
+    if request.method == 'DELETE':
+        delete_user(db, user_exists)
+        return no_content()
+
+@users_blueprint.route('/users/', methods=['GET', 'POST'])
+@validate_json(ignore_method=['GET'])
+def users():
+    if request.method == 'GET':
+        users = get_users(request)
+        return ok(users)
+
+    if request.method == 'POST':
+        body = request.get_json()
+        user = create_user(db, body)
+        return created(user)
